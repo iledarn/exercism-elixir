3f096a82e3a3ce6e379f874cc652614387e30159
iledarn <iledarn@gmail.com>
2022-05-09 23:07:41 +0300

    basketball-website: task 1

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,6 +1,15 @@
 defmodule BasketballWebsite do
+  def extract_from_path(nil, _) do
+    nil
+  end
+
+  def extract_from_path(data, nil) do
+    data
+  end
+
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    [head | tail] = String.split(path, ".", parts: 2)
+    extract_from_path(data[head], List.last(tail))
   end
 
   def get_in_path(data, path) do
