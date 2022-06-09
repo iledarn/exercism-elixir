25aa1266ceae87cb132da137e96fcc5ced2f49af
iledarn <iledarn@gmail.com>
2022-06-09 13:48:33 +0300

    file-sniffer: task 2

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -10,8 +10,24 @@ defmodule FileSniffer do
     end
   end
 
-  def type_from_binary(file_binary) do
-    # Please implement the type_from_binary/1 function
+  def type_from_binary(<<0x7F, 0x45, 0x4C, 0x46, _::binary>>) do
+    "application/octet-stream"
+  end
+
+  def type_from_binary(<<0x42, 0x4D, _::binary>>) do
+    "image/bmp"
+  end
+
+  def type_from_binary(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>>) do
+    "image/png"
+  end
+
+  def type_from_binary(<<0xFF, 0xD8, 0xFF, _::binary>>) do
+    "image/jpg"
+  end
+
+  def type_from_binary(<<0x47, 0x49, 0x46, _::binary>>) do
+    "image/gif"
   end
 
   def verify(file_binary, extension) do
